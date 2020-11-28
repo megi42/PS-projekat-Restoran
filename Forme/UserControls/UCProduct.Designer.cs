cf7b724dcf467faad1b7bfea175b1eed11a006d0
megi42 <megicar998@gmail.com>
2020-11-28 13:42:03 +0100

    commit

--- Forme/UserControls/UCProduct.Designer.cs
+++ Forme/UserControls/UCProduct.Designer.cs
@@ -35,7 +35,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Name = "UCProduct";
-            this.Size = new System.Drawing.Size(204, 150);
+            this.Size = new System.Drawing.Size(204, 184);
             this.ResumeLayout(false);
 
         }
